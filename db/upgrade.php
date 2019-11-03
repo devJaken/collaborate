405dd131017c5b71a8d1e9b8beca578e20c8870d
Codepro24 <prateekchatter@gmail.com>
2019-11-04 10:55:40 +1100

    added php code to upgrade.php

--- db/upgrade.php
+++ db/upgrade.php
@@ -66,6 +66,21 @@ function xmldb_collaborate_upgrade($oldversion) {
 
         upgrade_mod_savepoint(true, 20190615023, 'collaborate');
     }
+	 if ($oldversion < 20190615023) {
+
+        // Define field id to be added to collaborate_submissions.
+        $table = new xmldb_table('collaborate_submissions');
+        $field = new xmldb_field('id', XMLDB_TYPE_INTEGER, '18', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null, null);
+
+        // Conditionally launch add field id.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        // Collaborate savepoint reached.
+        upgrade_mod_savepoint(true, 20190615023, 'collaborate');
+    }
+
 
     return true;
 }
