c7e95e9402913464ee79cb23151ad4823bf716df
Codepro24 <prateekchatter@gmail.com>
2019-11-18 11:14:12 +1100

    Week7 Task2

---
+++ classes/task/collaborate_adhoc.php
@@ -0,0 +1,48 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>;.
+/**
+ * Class providing custom editors
+ *
+ * @package   mod_simplelesson
+ * @copyright 2018 Richard Jones https://richardnz.net
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+namespace mod_collaborate\task;
+defined('MOODLE_INTERNAL') || die();
+require_once($CFG->dirroot . '/mod/collaborate/lib.php');
+/**
+ * A adhoc task.
+ *
+ * @package    mod_collaborate
+ * @since      Moodle 2.7
+ * @copyright  2015 Flash Gordon http://www.flashgordon.com
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class collaborate_adhoc
+extends \core\task\adhoc_task {
+
+
+     /**
+     *  Run all the tasks
+     */
+     public function execute(){
+        $trace = new \text_progress_trace();
+        $cd =  $this->get_custom_data();;
+            collaborate_do_adhoc_task($trace, $cd);
+    }
+}
